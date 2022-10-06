57f5293676100e1ac63f84504c5e8c657b448d33
Jaiwincr7 <chaudharijr@rknec.edu>
2022-10-06 04:17:22 -0700

    1

---
+++ js.js
@@ -0,0 +1,27 @@
+var swiper = new Swiper(".mySwiper", {
+    speed:400,
+      effect: 'cube',
+      cubeEffect: {
+        slideShadows: false,
+      },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  var swiper = new Swiper(".mySwiper1", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
